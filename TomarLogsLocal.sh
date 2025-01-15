a332aa1132b25bc69f21e440ba6c7fdb49b366fa
Xelofrost <horaciodcm@gmail.com>
2025-01-15 20:59:17 +0100

    Toma de Logs actualizada para logs cowrie

--- TomarLogsLocal.sh
+++ TomarLogsLocal.sh
@@ -6,9 +6,6 @@ read -p "Introduce la IP del servidor remoto: " SSH_HOST
 # Pedir el nombre de usuario del servidor remoto
 read -p "Introduce el nombre de usuario del servidor remoto: " SSH_USER
 
-# Ruta de los logs en el servidor remoto
-read -p "Introduce la ruta de los logs en el servidor remoto: " SSH_PATH
-
 # Ruta local donde se guardarán los logs
 LOCAL_PATH="./logs_ssh"    # Ruta local donde se guardarán los logs
 
@@ -20,7 +17,7 @@ fi
 
 # Comando SCP para copiar los logs desde el servidor remoto usando el puerto 777
 echo "Copiando los logs desde el servidor remoto..."
-scp -P 777 "$SSH_USER@$SSH_HOST:$SSH_PATH/*.log" "$LOCAL_PATH"
+scp -P 777 "$SSH_USER@$SSH_HOST:/home/cowrie/cowrie/var/log/cowrie/*.log" "$LOCAL_PATH"
 
 # Comprobar si el comando SCP fue exitoso
 if [ $? -eq 0 ]; then
